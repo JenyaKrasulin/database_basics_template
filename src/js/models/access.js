ec526f860381b58d3aa0104cee9e8d6b733e7737
anrikolo <65027008+anrikolo@users.noreply.github.com>
2020-12-13 00:23:16 +0300

    Update access.js

--- src/js/models/access.js
+++ src/js/models/access.js
@@ -1,1 +1,32 @@
-sss
+const connection = require('../connection');
+const Sequilize = require('sequelize');
+
+class User extends Sequilize.Model {}
+
+User.init(
+    {
+        id: {
+            type: Sequilize.INTEGER,
+            primaryKey: true
+        },
+        login: {
+            type: Sequilize.STRING(100),
+            allowNull: false
+        },
+        password: {
+            type: Sequilize.STRING(50),
+            allowNull: false
+        },
+        email: {
+            type: Sequilize.STRING(100),
+            allowNull: false
+        },
+    },
+    {
+        sequelize: connection,
+        freezeTableName: true,
+        modelName: 'User'
+    }
+);
+
+module.exports = User
